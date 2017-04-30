14f4914f0aa8f3d5ac071344c678386a2ed6fe4f
dylanito2 <dylanito2@gmail.com>
2017-04-30 19:12:25 -0400

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,9 @@
-# Enter your procedural solution here!
+def collect_multiples(limit)
+  [*1...limit].select do |int|
+    int % 3 == 0 || int % 5 == 0
+  end
+end
+
+def sum_multiples(limit)
+  collect_multiples(limit).reduce(:+)
+end
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,17 @@
-# Enter your object-oriented solution here!
+class Multiples
+
+  def initialize(limit)
+    @limit = limit
+  end
+
+  def collect_multiples
+    [*1...@limit].select do |int|
+      int % 3 == 0 || int % 5 == 0
+    end
+  end
+
+  def sum_multiples
+    collect_multiples.reduce(:+)
+  end
+
+end
