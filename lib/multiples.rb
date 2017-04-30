a5e005d6734cbccc68f08b353a3d147ebc023ada
github username <github email address>
2017-04-30 18:16:19 -0400

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,14 @@
-# Enter your procedural solution here!
+##Find all multiples of 3 or 5 below a given integer
+
+def collect_multiples(limit)
+  range = (1...limit)
+  range.map do |number|
+    if number % 5 === 0 || number % 3 === 0
+      number
+    end
+  end.compact
+end
+
+def sum_multiples(limit)
+  collect_multiples(limit).reduce(:+)
+end
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,22 @@
-# Enter your object-oriented solution here!
+##Find all multiples of 3 or 5 below a given integer
+
+class Multiples
+  def initialize(limit)
+    @limit = limit
+    @multiples = collect_multiples
+  end
+
+  def collect_multiples
+    range = (1...@limit)
+    range.map do |number|
+      if number % 3 === 0 || number % 5 === 0
+        number
+      end
+    end.compact
+  end
+
+    def sum_multiples
+      @multiples.reduce(:+)
+    end
+
+end
