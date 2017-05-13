4bc656862269e3c514a607401df2f34457bae45c
fakebenjay <fakebenjay@gmail.com>
2017-05-13 13:40:56 -0400

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,17 @@
-# Enter your procedural solution here!
+def collect_multiples(n)
+  array = (1...n).to_a
+  mult_array = []
+
+  array.each do |a|
+    if a % 3 == 0 || a % 5 == 0
+      mult_array << a
+    end
+  end
+
+  return mult_array
+end
+
+def sum_multiples(n)
+  array = collect_multiples(n)
+  array.inject { |sum, i| sum + i }
+end
