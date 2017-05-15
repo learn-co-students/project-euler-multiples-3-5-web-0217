e73cba46714b5eb5d8ccd2c17606b01e6649922b
hwalborn <hwalborn@gmail.com>
2017-05-15 08:58:29 -0400

    Oiler

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,14 @@
 # Enter your procedural solution here!
+def collect_multiples(number)
+  arr = []
+  (1...number).each do |num|
+    if(num % 3 == 0 || num % 5 == 0)
+      arr.push(num)
+    end
+  end
+  return arr
+end
+
+def sum_multiples(limit)
+  collect_multiples(limit).reduce(:+)
+end
